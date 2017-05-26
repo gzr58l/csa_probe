77f122fbb62d31e666647e5307fb1f8d77648fbd
Carl Kanzabedian <carl.kanzabedian@gmail.com>
2017-05-26 00:03:26 -0500

    Working rest call

--- rest_test.js
+++ rest_test.js
@@ -1,15 +1,32 @@
 var Client = require('node-rest-client').Client;
  
-var client = new Client();
+var options = {
+                mimetypes: {
+                        json: ["application/json", "application/json;charset=utf-8"],
+                        xml: ["application/xml", "application/xml;charset=utf-8"]  
+                    },
+                user: "",
+                password:""
+                };
+
+
+var client = new Client(options);
  
 // direct way 
-client.get("http://remote.site/rest/xml/method", function (data, response) {
+client.get("https://jsonplaceholder.typicode.com/posts/1", function (data, response) {
     // parsed response body as js object 
     console.log(data);
     // raw response 
-    console.log(response);
+    //console.log(response);
+}).on('error', function (err) {
+    console.log('something went wrong on the request', err.request.options);
 });
  
+// handling client error events 
+client.on('error', function (err) {
+    console.error('Something went wrong on the client', err);
+});
+
 // registering remote methods 
 client.registerMethod("jsonMethod", "http://remote.site/rest/json/method", "GET");
  
@@ -17,5 +34,5 @@ client.methods.jsonMethod(function (data, response) {
     // parsed response body as js object 
     console.log(data);
     // raw response 
-    console.log(response);
+    //console.log(response);
 });
